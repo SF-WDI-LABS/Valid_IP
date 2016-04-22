aba9c5b2873f0020f6efe6473f1a01d2cf0ea4a3
Brianna Veenstra <b.g.veenstra@gmail.com>
2016-04-21 17:28:02 -0700

    more tests!

--- solution-code/solution.rb
+++ solution-code/solution.rb
@@ -1,14 +1,22 @@
+
 def validIP?(address)
   # default value of valid is true
   valid = true
-  # split the address sting by periods
-  split_address = address.split(".")
-  # get the length of the address
-  address_length = split_address.length
-  # set valid to false if the address' length is > 6 or < 4
-  valid = false if address_length > 6 || address_length < 4
-  # iterate through the address
-  split_address.each do |num, i|
+  # split the address string by periods
+  address_segments = address.split(".")
+  # get the number of segments
+  address_length = address_segments.length
+  # set valid to false if the number of segments is > 6 or < 4
+  valid = false if address_length != 4
+  # iterate through the address segments
+  address_segments.each do |num, i|
+    # mark invalid if a segment is empty
+    valid = false if num.empty?
+    # check that address segments have only digits in them
+    num.each_char do |c|
+      # if we find a forbidden character, set valid to false
+      valid = false if !"0123456789".include? c
+    end
     # convert each item to a number
     num = num.to_i
     # check if any number is greater than 255 or less than 0
