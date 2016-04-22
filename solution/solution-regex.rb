f7200f5ec9931562cf609d9ffb6225f1d40ab91f
Nathan Allen <thenathanator@gmail.com>
2016-04-22 15:53:30 -0700

    accepted regex solution

--- solution/solution-regex.rb
+++ solution/solution-regex.rb
@@ -1,10 +1,3 @@
-# a common solution we saw in class, close!
-def valid_ish_IP?(address)
-  !!address.match(/^([0-255]{1,3}\.){3}[0-255]{1,3}$/)
-end
-
-
-# more accurate:
 def validIP?(address)
   !!address.match(/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/) 
 end
